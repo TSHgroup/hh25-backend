d549db0ae15bffa83d9a0dd6852e7def99f541ca
ggod <46885632+GGodPL@users.noreply.github.com>
2025-11-18 13:38:12 +0100

    feat: add scenario creation

---
+++ src/models/ScenarioModels.ts
@@ -0,0 +1,34 @@
+import z from 'zod';
+import languages from '../../data/languages.json';
+import models from '../../data/models.json';
+
+export const ScenarioBody = z.object({
+    title: z.string()
+        .min(1)
+        .max(130),
+    subtitle: z.string()
+        .min(1)
+        .max(200)
+        .optional(),
+    description: z.string()
+        .min(1)
+        .max(512)
+        .optional(),
+    category: z.literal(['business', 'education', 'relationships', 'family', 'dates', 'public speaking']),
+    tags: z.array(z.string().min(1).max(50))
+        .min(0)
+        .max(5)
+        .optional(),
+    languages: z.array(z.literal(languages.map(l => l.code)))
+        .min(1)
+        .max(5)
+        .optional(),
+    status: z.literal(['draft', 'editing', 'review', 'published', 'archived', 'deleted']),
+    objectives: z.array(z.string().min(1).max(200))
+        .min(1)
+        .max(5),
+    persona: z.hex()
+        .length(24),
+    provider: z.literal(Object.keys(models)),
+    model: z.literal(Object.values(models).flat()),
+});
--- src/routes/scenario/index.ts
+++ src/routes/scenario/index.ts
@@ -2,9 +2,48 @@ import { Router } from 'express';
 
 import userRouter from './user';
 import user from '../../middlewares/user';
+import Scenarios from '../../mongodb/Scenarios';
+import { validateBody } from '../../middlewares/validate';
+import { ScenarioBody } from '../../models/ScenarioModels';
+
+import models from '../../../data/models.json';
 
 const router = Router();
 
 router.use('/user', user(), userRouter);
+
+router.post('/', user(), validateBody(ScenarioBody), async (req, res) => {
+    const { title, subtitle, description, category, tags, languages, status, objectives, persona, openingPrompt, closingPrompt, provider, model } = req.body;
+
+    if (!(models as Record<string, string[]>)[provider].includes(model)) {
+        res.status(400).send({
+            error: "Invalid model"
+        });
+        return;
+    }
+
+    const scenario = await Scenarios.insertOne({
+        createdBy: req.user!._id,
+        title,
+        subtitle,
+        description,
+        category,
+        tags,
+        languages,
+        status,
+        lastUpdatedAt: new Date(),
+        objectives,
+        persona,
+        openingPrompt,
+        closingPrompt,
+        ai: {
+            provider,
+            model,
+        },
+        rounds: [],
+    });
+
+    res.send(scenario);
+});
 
 export default router;
